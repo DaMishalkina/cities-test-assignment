883e749d81832cf4efe76679555ab166d1129cc5
Daria Mishalkina <dashaio251@gmail.com>
2023-08-05 13:52:53 +0200

    add api endpoint for landmarks

--- server/server.js
+++ server/server.js
@@ -9,11 +9,28 @@ fs.readFile("./cities.json", "utf8", (err,data) => {
         console.error(err);
     }
     cities = JSON.parse(data);
+    cities["cities"] = cities["cities"].map(city => {
+        city.id = city["name"].toLowerCase().replace(/\s/g, "-");
+        return city
+    })
+
 })
 
 app.get("/cities", (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
     res.json(cities);
+})
+
+app.get("/landmarks/:id", (req, res) => {
+    const id = req.params.id;
+    res.set('Access-Control-Allow-Origin', '*');
+    let landmarks = [];
+    cities["cities"].forEach(city => {
+        if(city.id === id){
+            landmarks = city.landmarks;
+        }
+    })
+    res.json(landmarks);
 })
 
 app.listen(8080, () => {console.log("Server started on port 8080")})
